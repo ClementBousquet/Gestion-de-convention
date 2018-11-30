03121dad34a7d0d23a2444efa344f11b235591ee
ltn1852a <yann.lattes@hotmail.fr>
2018-11-30 14:26:54 +0100

    main

--- ServiceJuridique/src/main/java/miage/project/servicejur/ServiceJuridiqueDaemon.java
+++ ServiceJuridique/src/main/java/miage/project/servicejur/ServiceJuridiqueDaemon.java
@@ -11,6 +11,7 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import javax.naming.NamingException;
 
 /**
  *
@@ -26,7 +27,16 @@ public class ServiceJuridiqueDaemon implements MessageListener {
 
                 if (o instanceof ServiceJuridiqueMessage) {
                     //Traitement
-                    Convention conv = ((ServiceJuridiqueMessage)o).getConvention();
+                   ServiceJuridiqueMessage sjm=(ServiceJuridiqueMessage) o;
+                   
+                   
+                   String statut="";
+                   PubJuridique pub=new PubJuridique(new ServiceJuridiqueMessage(sjm,statut));
+                    try {
+                        pub.main();
+                    } catch (NamingException ex) {
+                        Logger.getLogger(ServiceJuridiqueDaemon.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             } catch (JMSException ex) {
                 Logger.getLogger(ServiceJuridiqueDaemon.class.getName()).log(Level.SEVERE, null, ex);
