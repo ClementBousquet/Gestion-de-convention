91b51adcd7cd36d9cb4244d1d97bf366b421e796
ltn1852a <yann.lattes@hotmail.fr>
2018-11-30 13:49:32 +0100

    Services Messages OK

--- ServiceAdministratif/src/main/java/miage/project/serviceadm/ServiceAdministratifMessage.java
+++ ServiceAdministratif/src/main/java/miage/project/serviceadm/ServiceAdministratifMessage.java
@@ -10,18 +10,100 @@ import java.io.Serializable;
  * @author yannl
  */
 public class ServiceAdministratifMessage implements Serializable {
-    String valeur;
-    int idEtudiant;
+    private String idConvention;
+    private int idEtudiant;
+    private String code;
+    private String intitule;
+    private String nom;
+    private String prenom;
+    private String statut;
+    private String niveau;
     
-    public ServiceAdministratifMessage(String valeur,int idEtudiant){
-        this.valeur=valeur;
+    public ServiceAdministratifMessage(String idConvention,int idEtudiant,String nom,String prenom,String code,String intitule,String niveau){
+        this.idConvention=idConvention;
         this.idEtudiant=idEtudiant;
-    }
-    public String getText(){
-        return this.valeur;
+        this.statut="En Cours";
+        this.nom=nom;
+        this.prenom=prenom;
+        this.code=code;
+        this.intitule=intitule;
+        this.niveau=niveau;
     }
     
-   public void setText(String value){
-       this.valeur=value;
+    public ServiceAdministratifMessage(ServiceAdministratifMessage sam,String statut){
+        this.idConvention=sam.getIdConvention();
+        this.statut=statut;     
+    }
+    
+    public String getidConvention(){
+        return this.idConvention;
+    }
+    
+   public void setValeur(String idConvention){
+       this.idConvention=idConvention;
    }
+   
+   public void setStatut(String statut){
+       this.statut=statut;
+   }
+   
+   public String getStatut(){
+       return this.statut;
+   }
+    public String getIdConvention() {
+        return idConvention;
+    }
+
+    public void setIdConvention(String idConvention) {
+        this.idConvention = idConvention;
+    }
+
+    public int getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(int idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+    
 }
--- ServicePedagogique/MiageServicePedagogique-ejb/src/main/java/miage/project/JMS/ServicePedagogiqueMessage.java
+++ ServicePedagogique/MiageServicePedagogique-ejb/src/main/java/miage/project/JMS/ServicePedagogiqueMessage.java
@@ -6,26 +6,98 @@
 package miage.project.JMS;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
  * @author yannl
  */
 public class ServicePedagogiqueMessage implements Serializable {
-    String valeur;
-    //Convention convention;
-    //String idEnseignement;
-    
-    public ServicePedagogiqueMessage(String valeur/*,Convention convention*/){
-        this.valeur=valeur;
-        //this.convention=convention;
-        //this.idEnseignement=idEnseignement;
+    private String idConvention;
+    private Date dateDebut;
+    private Date dateFin;
+    private String statut;
+    private String resume;
+    private String intule;
+    private String niveau;
+    private String prof_ref;
+
+    public ServicePedagogiqueMessage(String idConvention, Date dateDebut, Date dateFin, String statut, String resume, String intule) {
+        this.idConvention = idConvention;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+        this.resume = resume;
+        this.intule = intule;
     }
-    public String getText(){
-        return this.valeur;
+     public ServicePedagogiqueMessage(ServicePedagogiqueMessage spm,String statut){
+         this.idConvention=spm.idConvention;
+         this.statut=statut;
+     }
+    public String getProf_ref() {
+        return prof_ref;
+    }
+
+    public void setProf_ref(String prof_ref) {
+        this.prof_ref = prof_ref;
+    }
+
+    public String getIdConvention() {
+        return idConvention;
+    }
+
+    public void setIdConvention(String idConvention) {
+        this.idConvention = idConvention;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public String getIntule() {
+        return intule;
+    }
+
+    public void setIntule(String intule) {
+        this.intule = intule;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
     }
     
-   public void setText(String value){
-       this.valeur=value;
-   }
+    
+    
 }
